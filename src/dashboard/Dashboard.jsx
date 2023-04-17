a03684483d0effdc1a364f9af5484292d4f1265a
akku-18 <akshanshgarg18@gmail.com>
2023-04-17 08:29:35 +0530

    added cards section

--- src/dashboard/Dashboard.jsx
+++ src/dashboard/Dashboard.jsx
@@ -4,10 +4,10 @@ import { BiUserCircle, BiLike } from "react-icons/bi";
 import { AiOutlinePieChart } from "react-icons/ai";
 import { AiOutlineSchedule } from "react-icons/ai";
 import { TbTags } from "react-icons/tb";
-import { BsSearch, BsBell } from "react-icons/bs";
+import { BsBell } from "react-icons/bs";
 import { AiOutlineSearch } from "react-icons/ai";
 import { FiUsers } from "react-icons/fi";
-import { AiFillCamera } from "react-icons/ai";
+import { BsCashCoin } from "react-icons/bs";
 import dp from "./Alan DP.png";
 import "./dashboard.css";
 
@@ -60,12 +60,35 @@ export default function Dashboard() {
             <img src={dp} alt="" className="dp" />
           </div>
         </div>
-        <div className="total-revenue">
-          <div className="revenue-left">
-          <div className="revenue">Total Revenue</div>
-          <span>&#36; 2,129,430</span>
+        <div className="cards">
+          <div className="card" id="revenue">
+            <div className="card-left">
+              <div className="card-heading">Total Revenue</div>
+              <span className="card-amount">&#36; 2,129,430</span>
+            </div>
+            <BsCashCoin className="card-svg" />
           </div>
-          <AiFillCamera className="camera" /> 
+          <div className="card" id="transaction">
+            <div className="card-left">
+              <div className="card-heading">Total Transaction</div>
+              <span className="card-amount">1,520</span>
+            </div>
+            <TbTags className="card-svg" />
+          </div>
+          <div className="card" id="likes">
+            <div className="card-left">
+              <div className="card-heading">Total Likes</div>
+              <span className="card-amount">9,721</span>
+            </div>
+            <BiLike className="card-svg" />
+          </div>
+          <div className="card" id="users">
+            <div className="card-left">
+              <div className="card-heading">Total Users</div>
+              <span className="card-amount">892</span>
+            </div>
+            <FiUsers className="card-svg" />
+          </div>
         </div>
       </div>
     </div>
